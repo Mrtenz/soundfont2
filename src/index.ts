98dbf8134b1abe93ebf5e07a13bab36baf4b1567
Mrtenz <maarten@zuidhoorn.com>
2019-01-13 17:03:07 +0100

    Don't expose internal methods

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-export * from './chunks';
-export * from './riff';
 export * from './types';
-export * from './utils';
 export * from './chunk';
 export * from './constants';
 export * from './soundFont2';
